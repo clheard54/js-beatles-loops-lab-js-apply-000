3eb7bd93ac0a94e272d5bb78e75fe8afeaef4d79
Clarion Heard <clahon54@gmail.com>
2019-11-03 14:49:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ johnLennonFacts(facts)
 function iLoveTheBeatles(n) {
   var newArray = []
   do {
-    newArray.push("I love the Beatles!")
-  }
+    newArray.push("I love the Beatles!");
+    n++;
+  } 
 }
