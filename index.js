ca7860ae615e6cca8959843e1717ca77111f727a
Clarion Heard <clahon54@gmail.com>
2019-11-03 14:06:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+var whatTheyPlay = []
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  
+function
+theBeatlesPlay(musicians, instruments) {
+  for (let counter = 0; counter < 4; counter++) {
+    whatTheyPlay.push("${musicians[counter]} plays ${instruments[counter]}")
+  }
+}
+
+function
+johnLennonFacts(facts) {
+  while ()
+}
