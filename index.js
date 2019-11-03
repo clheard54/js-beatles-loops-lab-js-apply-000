fb3ab11e707fa6ea34612bfe7e235f751bb36fb2
Clarion Heard <clahon54@gmail.com>
2019-11-03 14:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ var whatTheyPlay = []
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   
-function
-theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   for (let counter = 0; counter < 4; counter++) {
     whatTheyPlay.push("${musicians[counter]} plays ${instruments[counter]}")
   }
 }
 
-function
-johnLennonFacts(facts) {
+function johnLennonFacts(facts) {
   var counter = 0
   while (counter<johonLennonFacts.length) {
     johnLennonFacts[counter]=johnLennonFacts[counter]+"!!!";
